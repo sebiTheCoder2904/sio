4252f570ae8612a974b4155572d2c2e2adad94a2
sebiTheCoder2904 <sebi@skschmid.de>
2025-07-10 17:37:36 +0100

    add files from external source

--- GamePanel.java
+++ GamePanel.java
@@ -7,17 +7,22 @@ import javax.imageio.ImageIO;
 
 public class GamePanel extends JPanel implements KeyListener, MouseMotionListener {
     private int x = 400, y = 300;
-    private int speed = 4;
+    private int xG = 500, yG = 400;
+    private int speed = 12;
     private boolean up, down, left, right;
     private Point mouse = new Point(0, 0);
     private BufferedImage playerImage;
+    private BufferedImage gegnerImage;
+    private BufferedImage groundImage;
 
     public GamePanel() {
         setFocusable(true);
         addKeyListener(this);
         addMouseMotionListener(this);
         try {
-            playerImage = ImageIO.read(new java.io.File("player.png"));
+            playerImage = ImageIO.read(new java.io.File("Spieler-removebg-preview.png"));
+            gegnerImage = ImageIO.read(new java.io.File("player.png"));
+            groundImage = ImageIO.read(new java.io.File("ground.png"));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -35,22 +40,48 @@ public class GamePanel extends JPanel implements KeyListener, MouseMotionListene
         if (down) y += speed;
         if (left) x -= speed;
         if (right) x += speed;
+
+        int dx = x - xG;
+        int dy = y - yG;
+        double distance = Math.sqrt(dx * dx + dy * dy);
+
+        // Normalize and move enemy slowly
+        if (distance > 1) {
+            double speedG = 2.0; // enemy speed
+            xG += (int)(speedG * dx / distance);
+            yG += (int)(speedG * dy / distance);
+        }
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D) g; // Spieler initialisieren und deklarieren
+        Graphics2D player = (Graphics2D) g; // Spieler initialisieren und deklarieren
+        Graphics2D gegner = (Graphics2D) g; // Spieler initialisieren und deklarieren 
+        Graphics2D ground = (Graphics2D) g; // Spieler initialisieren und deklarieren 
+        
+        ground.translate(500, 500); 
+        ground.translate(-groundImage.getWidth() / 2, -groundImage.getHeight() / 2);
+        ground.drawImage(groundImage, 0, 0, null);
+        // ground.scale(20000, 20000);
 
         
         double angle = Math.atan2(mouse.y - y, mouse.x - x);  //winkel zur maus berechnen
-        
-        AffineTransform old = g2.getTransform();
-        g2.translate(x, y); 
-        g2.rotate(angle);
-        g2.translate(-playerImage.getWidth() / 2, -playerImage.getHeight() / 2);
-        g2.drawImage(playerImage, 0, 0, null);
-        g2.setTransform(old);
+        double angleG = Math.atan2(yG - y, xG - x);
+
+        AffineTransform old = player.getTransform();
+        player.translate(x, y); 
+        player.rotate(angle);
+        player.translate(-playerImage.getWidth() / 8, -playerImage.getHeight() / 8);
+        player.drawImage(playerImage, 0, 0, 573/4, 435/4, null);
+        player.setTransform(old);
+
+        AffineTransform oldGegner = gegner.getTransform();
+        gegner.translate(xG, yG); 
+        gegner.rotate(angleG + 9.2);
+        gegner.translate(-gegnerImage.getWidth() / 4, -gegnerImage.getHeight() / 4);
+        gegner.drawImage(gegnerImage, 0, 0, 250/2, 165/2, null);
+        gegner.setTransform(oldGegner);
     }
 
     // Controls
@@ -70,9 +101,7 @@ public class GamePanel extends JPanel implements KeyListener, MouseMotionListene
         if (code == KeyEvent.VK_D) right = false;
     }
 
-    
     public void keyTyped(KeyEvent e) {} // exit method
-
     public void mouseMoved(MouseEvent e) 
     { 
         mouse = e.getPoint(); 
@@ -81,5 +110,5 @@ public class GamePanel extends JPanel implements KeyListener, MouseMotionListene
     public void mouseDragged(MouseEvent e) {
         mouseMoved(e);
     }
-    
+
 }
